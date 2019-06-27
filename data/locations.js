f86b86c37c74c7f5c4b3c71ac181196d62d1b7fa
Ans de Nijs <ansdenijs@gmail.com>
2019-06-27 22:08:52 +0200

    Update locations

--- data/locations.js
+++ data/locations.js
@@ -2,6 +2,12 @@ const locations = [
   // USA
   { city: 'New York', coordinates: [40.712776, -74.005974] },
   { city: 'Orlando', coordinates: [28.538336, -81.379234] },
+  { city: 'Miami', coordinates: [25.761681, -80.191788] },
+  { city: 'Everglades', coordinates: [25.18045, -80.791908] },
+
+  // Hawaii
+  { city: 'Big Island', coordinates: [19.542915, -155.665863] },
+  { city: 'Kauai', coordinates: [22.096439, -159.526123] },
   { city: 'Honolulu', coordinates: [21.306944, -157.858337] },
 
   // England
@@ -9,8 +15,8 @@ const locations = [
   { city: 'London', coordinates: [51.507351, -0.127758] },
   { city: 'Edinburgh', coordinates: [55.953251, -3.188267] },
   { city: 'Oxford', coordinates: [51.752022, -1.257726] },
-  { city: 'Bristol', coordinates: [36.595104, -82.188744] },
-  { city: 'Gloucester', coordinates: [42.615929, -70.661987] },
+  { city: 'Bristol', coordinates: [51.455311, -2.5919] },
+  { city: 'Gloucester', coordinates: [51.866741, -2.24867] },
   { city: 'Bath', coordinates: [51.38488, -2.36197] },
   { city: 'Salisbury', coordinates: [51.067402, -1.79763] },
   { city: 'Newcastle upon Tyne', coordinates: [54.978252, -1.61778] },
@@ -24,17 +30,41 @@ const locations = [
   { city: 'Berlin', coordinates: [52.520008, 13.404954] },
   { city: 'Kevelaer', coordinates: [51.582062, 6.24805] },
   { city: 'Düsseldorf', coordinates: [51.227741, 6.773456] },
+  { city: 'Köln', coordinates: [50.937531, 6.960279] },
   { city: 'Aachen', coordinates: [50.775429, 6.08149] },
   { city: 'Antwerp', coordinates: [51.219448, 4.402464] },
   { city: 'Paris', coordinates: [48.856613, 2.352222] },
-  { city: 'Nice', coordinates: [43.7009358, 7.2683912] },
+  { city: 'Nice', coordinates: [43.710175, 7.261953] },
   { city: 'Rome', coordinates: [41.902782, 12.496365] },
   { city: 'Valencia', coordinates: [39.470242, -0.3768] },
   { city: 'Faro', coordinates: [37.019356, -7.93044] },
+  { city: 'Florence', coordinates: [43.769562, 11.255814] },
+  {
+    city: 'San Jacopo al Girone',
+    coordinates: [43.769928, 11.339814]
+  },
+  { city: 'Perugia', coordinates: [43.110718, 12.390828] },
+  { city: 'Pisa', coordinates: [43.722839, 10.401689] },
+  {
+    city: 'Cascata delle Marmore Belvedere Superiore',
+    coordinates: [42.551162, 12.715248]
+  },
+  { city: 'San Gimignano', coordinates: [43.470329, 11.04376] },
+  { city: 'Castellina in Chianti', coordinates: [43.471359, 11.28537] },
+  { city: 'Siena', coordinates: [43.31881, 11.330757] },
+  { city: 'Lucca', coordinates: [43.83762, 10.495061] },
+  { city: 'Orvieto', coordinates: [42.716469, 12.11157] },
+  { city: 'Lago Trasimeno', coordinates: [43.137693, 12.102642] },
+  { city: 'Bagnoregio', coordinates: [42.62698, 12.090872] },
   { city: 'Catania', coordinates: [37.507877, 15.08303] },
+  { city: 'Agios Nikolaos', coordinates: [35.189018, 25.71707] },
   { city: 'Helsinki', coordinates: [60.169857, 24.938379] },
   { city: 'Rovaniemi', coordinates: [66.503944, 25.729391] },
   { city: 'Oulu', coordinates: [65.012093, 25.465076] },
+  { city: 'Kemi', coordinates: [65.736092, 24.563629] },
+  { city: 'Pyhä-Luoston', coordinates: [67.165403, 26.909171] },
+  { city: 'Ranua', coordinates: [57.330683, 25.612312] },
+  { city: 'Olkkajärvi', coordinates: [66.58773, 26.045143] },
 
   // Russia
   { city: 'Moscow', coordinates: [55.755825, 37.617298] },
