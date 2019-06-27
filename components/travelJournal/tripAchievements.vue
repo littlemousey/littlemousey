b3e4e25e908a91eee18a354943f8dfbb2e4dd04d
Ans de Nijs <ansdenijs@gmail.com>
2019-06-27 14:27:00 +0200

    Implemented components

---
+++ components/travelJournal/tripAchievements.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-layout row wrap align-start justify-center>
+    <v-flex xs12 sm10 md4 my-5>
+      <v-card>
+        <v-toolbar color="blue darken-4" dark>
+          <v-toolbar-title>Achievements 🐭</v-toolbar-title>
+        </v-toolbar>
+        <v-list>
+          <v-list-tile
+            v-for="achievement in achievements"
+            :key="achievement.title"
+            avatar
+          >
+            <v-list-tile-action>
+              <v-icon v-if="achievement.achieved" color="red">star</v-icon>
+            </v-list-tile-action>
+
+            <v-list-tile-content>
+              <v-list-tile-title v-text="achievement.title"></v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  name: 'TripAchievements',
+  props: {
+    achievements: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
--- pages/travels/schiermonnikoog.vue
+++ pages/travels/schiermonnikoog.vue
@@ -13,133 +13,58 @@
       text="The ferry to the island. It takes about 45 minutes. You can buy
           tickets online. It would be wise to check the timetable departure
           times."
+      :image-left="true"
     ></trip-highlight>
-    <v-layout row wrap my-5>
-      <v-flex xs5 mx-2 align-self-center>
-        <div>
-          There are many companies for renting bikes to get around the island
-          quickly.
-        </div>
-      </v-flex>
-      <v-flex xs5 mx-2>
-        <v-card>
-          <v-img src="https://i.postimg.cc/TYjC8NPK/DSC05121.jpg"></v-img>
-        </v-card>
-      </v-flex>
-    </v-layout>
-    <v-layout row wrap my-5>
-      <v-flex xs5 mx-2>
-        <v-card>
-          <v-img src="https://i.postimg.cc/LsQxCq8v/IMAG3564.jpg"></v-img>
-        </v-card>
-      </v-flex>
-      <v-flex xs5 mx-2 align-self-center>
-        <div>
-          The town center contains one supermarket and one bakery, but also
-          offers a few shops and there is plenty of choice for restaurants.
-        </div>
-      </v-flex>
-    </v-layout>
-    <v-layout row wrap my-5>
-      <v-flex xs5 mx-2>
-        <v-card>
-          <v-img src="https://i.postimg.cc/mDGF02bn/IMAG3614.jpg"></v-img>
-        </v-card>
-      </v-flex>
-      <v-flex xs5 mx-2 align-self-center>
-        <div>
-          One well-known hotel and restaurant is called 'Hotel Van Der Werff'.
-        </div>
-      </v-flex>
-    </v-layout>
-    <v-layout row wrap my-5>
-      <v-flex xs5 mx-2>
-        <v-card>
-          <v-img src="https://i.postimg.cc/qq4yCBSK/IMAG3538.jpg"></v-img>
-        </v-card>
-      </v-flex>
-      <v-flex xs5 mx-2 align-self-center>
-        <div>
-          I can definitely recommend the 'Eilander appeltaart' (Island apple
-          pie) at restaurant Ambrosijn 😉🍰
-        </div>
-      </v-flex>
-    </v-layout>
-    <v-layout row wrap my-5>
-      <v-flex xs5 mx-2 align-self-center> </v-flex>
-      <v-flex xs5 mx-2>
-        <v-card>
-          <v-img src="https://i.postimg.cc/zBL09JLN/IMAG3562.jpg"></v-img>
-        </v-card>
-      </v-flex>
-    </v-layout>
-    <v-layout row wrap my-5>
-      <v-flex xs5 mx-2>
-        <v-card>
-          <v-img src="https://i.postimg.cc/bwGFm39G/DSC05112.jpg"></v-img>
-        </v-card>
-      </v-flex>
-      <v-flex xs5 mx-2 align-self-center> </v-flex>
-    </v-layout>
-    <v-layout row wrap my-5>
-      <v-flex xs5 mx-2 align-self-center>
-        <div>
-          Because the island is a breeding ground for birds, you can do bird
-          spotting from a wooden hut at the west lake to not disturb the birds.
-        </div></v-flex
-      >
-      <v-flex xs5 mx-2>
-        <v-card>
-          <v-img src="https://i.postimg.cc/13JBXgwP/DSC05152.jpg"></v-img>
-        </v-card>
-      </v-flex>
-    </v-layout>
-    <v-layout row wrap my-5>
-      <v-flex xs5 mx-2>
-        <v-card>
-          <v-img src="https://i.postimg.cc/zGJpqwL0/IMAG3570.jpg"></v-img>
-        </v-card>
-      </v-flex>
-      <v-flex xs5 mx-2 align-self-center>
-        <div>
-          The island famous monk, Martin van Waning. He was a sculptor and
-          artist.
-        </div></v-flex
-      >
-    </v-layout>
-    <v-layout row wrap my-5>
-      <v-flex xs5 mx-2 align-self-center>
-        <div>
-          The beaches are very wide and you might be able to spot seals. Be
-          cautious, you are not allowed to touch or come near young seals.
-        </div></v-flex
-      >
-      <v-flex xs5 mx-2>
-        <v-card>
-          <v-img src="https://i.postimg.cc/zvbdXkXb/DSC05179.jpg"></v-img>
-        </v-card>
-      </v-flex>
-    </v-layout>
-    <v-layout row wrap align-start justify-center>
-      <v-flex xs12 sm10 md4 my-5>
-        <v-card>
-          <v-toolbar color="amber darken-3" dark>
-            <v-toolbar-title>Achievements 🐭</v-toolbar-title>
-          </v-toolbar>
-          <v-list>
-            <v-list-tile v-for="item in items" :key="item.title" avatar>
-              <v-list-tile-action>
-                <v-icon v-if="item.achieved" color="red">star</v-icon>
-              </v-list-tile-action>
-
-              <v-list-tile-content>
-                <v-list-tile-title v-text="item.title"></v-list-tile-title>
-              </v-list-tile-content>
-            </v-list-tile>
-          </v-list>
-        </v-card>
-      </v-flex>
-    </v-layout>
+    <trip-highlight
+      image-url="https://i.postimg.cc/TYjC8NPK/DSC05121.jpg"
+      text="There are many companies for renting bikes to get around the island
+          quickly."
+      :image-left="false"
+    ></trip-highlight>
+    <trip-highlight
+      image-url="https://i.postimg.cc/LsQxCq8v/IMAG3564.jpg"
+      text="The town center contains one supermarket and one bakery, but also
+          offers a few shops and there is plenty of choice for restaurants."
+      :image-left="true"
+    ></trip-highlight>
+    <trip-highlight
+      image-url="https://i.postimg.cc/mDGF02bn/IMAG3614.jpg"
+      text="One well-known hotel and restaurant is called 'Hotel Van Der Werff'."
+      :image-left="true"
+    ></trip-highlight>
+    <trip-highlight
+      image-url="https://i.postimg.cc/qq4yCBSK/IMAG3538.jpg"
+      text="I can definitely recommend the 'Eilander appeltaart' (Island apple
+          pie) at restaurant Ambrosijn 😉🍰"
+      :image-left="true"
+    ></trip-highlight>
+    <trip-highlight
+      image-url="https://i.postimg.cc/zBL09JLN/IMAG3562.jpg"
+      :image-left="false"
+    ></trip-highlight>
+    <trip-highlight
+      image-url="https://i.postimg.cc/bwGFm39G/DSC05112.jpg"
+      :image-left="true"
+    ></trip-highlight>
+    <trip-highlight
+      image-url="https://i.postimg.cc/13JBXgwP/DSC05152.jpg"
+      text="Because the island is a breeding ground for birds, you can do bird
+          spotting from a wooden hut at the west lake to not disturb the birds."
+      :image-left="false"
+    ></trip-highlight>
+    <trip-highlight
+      image-url="https://i.postimg.cc/zGJpqwL0/IMAG3570.jpg"
+      text="The island famous monk, Martin van Waning. He was a sculptor and
+          artist."
+      :image-left="true"
+    ></trip-highlight>
+    <trip-highlight
+      image-url="https://i.postimg.cc/zvbdXkXb/DSC05179.jpg"
+      text="The beaches are very wide and you might be able to spot seals. Be
+          cautious, you are not allowed to touch or come near young seals."
+      :image-left="false"
+    ></trip-highlight>
+    <trip-achievements :achievements="activities"></trip-achievements>
     <v-layout row wrap align-start justify-center>
       <v-flex xs6 my-5>
         <div class="text-xs-center">
@@ -153,12 +78,13 @@
 <script>
 import TripHeader from '@/components/travelJournal/TripHeader'
 import TripHighlight from '@/components/travelJournal/TripHighlight'
+import TripAchievements from '@/components/travelJournal/TripAchievements'
 export default {
   name: 'Schiermonnikoog',
-  components: { TripHeader, TripHighlight },
+  components: { TripHeader, TripHighlight, TripAchievements },
   data() {
     return {
-      items: [
+      activities: [
         {
           achieved: true,
           title: 'Visit lighthouse'
